8c6ae7e648376321b94b46c1bc6e3076791cf0de
bogdan <bogdan_bibina@yahoo.com>
2021-03-07 01:57:35 -0800

    modify in AI detection

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import gensim
 from gensim.summarization import keywords
 from gensim.summarization.summarizer import summarize
 import json
+from nltk.stem.lancaster import LancasterStemmer
 from bs4 import BeautifulSoup
 import re
+import nltk
+from nltk.corpus import stopwords
+import requests
 
 ##https://medium.com/thecyphy/generating-abstractive-summaries-using-googles-pegasus-model-18eef8ae985b
 app = Flask(__name__)
@@ -25,11 +29,18 @@ def apiScraper():
         getType=int(data1['type'])
         data=googleAPIinteract(getURL,getType)
         extract=data.getJsonData()  
-        getdata=data.findPage(extract['items'][0]['link'])
-        return jsonify(getdata)
+        ##print(extract)
+        ##getdata=data.findPage(extract['items'][0]['snippet'])
+        return jsonify(extract)
 
 @app.route("/api/rewrite",methods=['POST'])
 def rewrite():
+        ##test
+        ##https://en.wikipedia.org/wiki/Gene_delivery
+        ##https://en.wikipedia.org/wiki/Biochemistry
+        ##bad 
+        ##https://en.wikipedia.org/wiki/Football_in_England
+
         data1=request.get_json()
         ##text1=data1["text"]
         addReturn=[]
@@ -38,7 +49,78 @@ def rewrite():
         bodyText = soup.get_text()
         get=KeyExtractor(bodyText)
         keys=get.extractorKEYS()
-        '''
+        f=open("static/data.txt","r")
+        text=f.readlines()
+        text=str(text)
+        a=[]
+        sum=""
+        for i in text:
+           if i!=',':
+             sum=sum+i
+           else:
+             for j in sum[1:-1].split(" "):                   
+                a.append(j)
+                sum=""
+        ##st = LancasterStemmer()
+        ##b=[]
+        ##for i in a:
+        ##   b.append(st.stem(i))
+        keySec=[]
+        sa=0
+        ##for i in a:
+        ##   print(i)
+        b=[]
+        stpWords=stopwords.words('english')
+        stpWords.append("ro")
+        stpWords.append("la")
+        stpWords.append("li")
+        stpWords.append("de")
+        stpWords.append("ea")
+        for i in a:
+           if i not in stpWords:
+                b.append(i)
+        a=b
+        for j in keys:
+           for il in a:     
+                
+              if il in j[0] :
+                 ##print(il,j[0])
+                 for z in keySec:
+                    if j[0]==z[0] :
+                      sa=1
+                      break
+                 if sa==0:
+                      keySec.append([j[0],j[1]])
+                 else:
+                      sa=0
+        ##print(keys,keySec)
+        keys=keySec
+        
+
+        if len(keys)>2000:
+                return jsonify(keys[:2000])
+        else:
+                return jsonify(keys)
+
+@app.route("/api/summary",methods=["POST"])
+def summaryCreate():
+        data1=request.get_json()
+        text=data1["text"]
+        ratio=float(data1["ratio"]/100)
+        text=summarize(str(text),ratio=ratio)
+        return text
+
+@app.route("/api/videos",methods=["POST","GET"])
+def videos():
+        data1=request.get_json()
+        text=data1["search"]
+        response = requests.get("https://www.googleapis.com/youtube/v3/search?q="+text+"&key=AIzaSyC9ZVMsS7RX4Temw5ORKoaaHQqw5BGb9RE")
+        return response.json()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+'''
         for text1 in data1["text"]:
                 ##print(text1)
                 add=0
@@ -63,21 +145,4 @@ def rewrite():
                 addReturn.append(save)
                 save=""
                 ##print(addReturn)
-        '''
-        if len(keys)>2000:
-                return jsonify(keys[:2000])
-        else:
-                return jsonify(keys)
-
-@app.route("/api/summary",methods=["POST"])
-def summaryCreate():
-        data1=request.get_json()
-        text=data1["text"]
-        ratio=float(data1["ratio"]/100)
-        text=summarize(str(text),ratio=ratio)
-        return text
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-
+'''
